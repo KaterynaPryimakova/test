31be93f2c4952bf0857dcfe6f0bc057312e18863
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-09-05 13:29:17 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -148,46 +148,4 @@
 //   // Значення властивості
 //   console.log(book[key]);
 // }
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "На березі спокійних вод",
-    author: "Роберт Шеклі",
-    rating: 8.51,
-  },
-  {
-    title: "Сон смішної людини",
-    author: "Федір Достоєвський",
-    rating: 7.75,
-  },
-];
-for (const book of books) {
-  // Об'єкт книги
-  //   console.log(book);
-  // Назва
-  console.log(book.title);
-  // Автор
-  //   console.log(book.author);
-  //   // Рейтинг
-  //   console.log(book.rating);
-}
-const bookNames = [];
-
-for (const book of books) {
-  bookNames.push(book.title);
-}
-
-console.log(bookNames);
-
-let totalRating = 0;
-
-for (const book of books) {
-  totalRating += book.rating;
-}
-
-const averageRating = (totalRating / books.length).toFixed(2);
-console.log(averageRating); // 8.2
+//
