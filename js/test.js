bdd79fd8ee2f03a8a010873521e622d6a32b7571
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-09-30 12:21:07 +0300

    +

--- js/test.js
+++ js/test.js
@@ -708,3 +708,252 @@
 //     return acc + product.price;
 //   }, 0);
 // console.log(sum);
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+//   static checkPrice(price) {
+//     if (price > Car.#MAX_PRICE) {
+//       return `Error! Price exceeds the maximum.`;
+//     }
+//     return `Success! Price is within acceptable limits`;
+//   }
+//   // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+// console.log(Car.checkPrice(64000));
+
+// Є об'єкт і функція для підрахунку суми чисел.
+// Виведіть у консоль результат виконання функції, якщо a = 3, b = 5.
+
+// var obj = { num: 2 };
+
+// function add(a, b) {
+//   return this.num + a + b;
+// }
+// console.log(add.call(obj, 3, 5));
+// console.log(add.apply(obj, [3, 5]));
+
+// const checkBind = add.bind(obj, 3, 5);
+// console.log(checkBind());
+
+// Є два об'єкти, які описують деякі характеристики телефонів.
+// Потрібно викликати метод showInfo, щоб він логував інформацію про кожний телефон,
+// передаючи аргументом країну-виробник телефону.
+// "HUAWEI" - "China", "SAMSUNG" - "South Korea".
+
+// const tel = {
+//   regNumber: "AE8345FEDAS",
+//   brand: "HUAWEI",
+//   showInfo(country) {
+//     console.log(`${country} ${this.regNumber} ${this.brand}`);
+//   },
+// };
+
+// const tel_2 = {
+//   regNumber: "AO8518FEGHT",
+//   brand: "SAMSUNG",
+// };
+
+// tel.showInfo(`China`);
+
+// const checkTel2 = tel.showInfo.bind(tel_2, `South Korea`);
+// checkTel2();
+
+// 1. Створіть об'єкт car з трьома властивостями
+// carBrand: "Audi Q3"
+// price: 23000
+// метод getData() котрий буде логувати рядок :"Audi Q3 за 23000 $"
+
+// 2. Напишіть функцію makeMessage(callback), котра отримує в якості параметра
+// callback-функцію getData() і логує повідомлення "Ви замовили ${callback()}"
+
+// const car = {
+//   carBrand: "Audi Q3",
+//   price: 23000,
+//   getData() {
+//     return `${this.carBrand} за ${this.price} $`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   console.log(`Ви замовили ${callback()}`);
+// }
+// makeMessage(car.getData.bind(car));
+
+// Напишіть код таким чином, щоб об'єкт apples став прототипом для об'єкта fuji.
+// Перевірте чи став об'єкт apples  прототипом для об'єкта fuji.
+// Додайте об'єкту fuji властивість color: 'pink'  і виведіть його у консоль.
+// Виведіть у консоль значення властивості type для об'єкту fuji.
+// Перевірте і виведіть у консоль чи є властивість vitamins власною властивістю об'єкту apples.
+// Перевірте і виведіть у консоль чи є властивість cookingMethod власною властивістю об'єкту fuji.
+// const apples = {
+//   type: "fruit",
+//   cookingMethod: ["jam", "juice", "baked", "dried", "fresh"],
+//   vitamins: ["A", "B1", "B2", "E"],
+// };
+// const fuji = Object.create(apples);
+// console.log(apples.isPrototypeOf(fuji));
+// fuji.color = "pink";
+// console.log(fuji.color);
+// console.log(fuji.type);
+// console.log(apples.hasOwnProperty`vitamins`);
+// console.log(fuji.hasOwnProperty`cookingMethod`);
+
+// Створи клас для калькулятора, який має такі методи:
+// метод number, який набуває початкового значення для наступних операцій
+// метод getResult, який повертає фінальний результат усіх операцій, проведених з числом
+// методи add, substruct, divide, multiply
+// Об'єкт класу приймає число і може проводити з ним
+// Послідовні операції у вигляді ланцюжка
+
+// class Calculator {
+//   constructor() {
+//     this.result = 0;
+//   }
+
+//   number(number) {
+//     this.result = number;
+//     return this;
+//   }
+//   add(number) {
+//     this.result += number;
+//     return this;
+//   }
+
+//   substruct(number) {
+//     this.result -= number;
+//     return this;
+//   }
+
+//   divide(number) {
+//     this.result /= number;
+//     return this;
+//   }
+//   multiply(number) {
+//     this.result *= number;
+//     return this;
+//   }
+//   getResult() {
+//     return this;
+//   }
+// }
+// const operation = new Calculator();
+// const result = operation.number(100).divide(5).getResult();
+// console.log(result);
+
+//Напиши клас Rectangle який створює об'єкт
+//з ​​властивостями height, width
+//і методом calculateArea() для підрахунку прлощі прямокутника.
+// class Rectangle {
+//   constructor(height, width) {
+//     this.height = height;
+//     this.width = width;
+//   }
+//   calculateArea() {
+//     return this.height * this.width;
+//   }
+// }
+// const exampl = new Rectangle(12, 10);
+// console.log(exampl.calculateArea());
+
+// Створіть клас Car, який має конструктор для зберігання марки та
+// року виробництва автомобіля. Додайте метод, який обчислює вік автомобіля
+// на основі поточного року та року виробництва і виведе в кнсоль рядок `Вік автомобіля: age років`
+
+// class Car {
+//   conctructor(model, year) {
+//     this.model = model;
+//     this.year = year;
+//   }
+//   calculAge(currentYear) {
+//     const age = currentYear - this.year;
+//     console.log(`Вік автомобіля: ${age} років`);
+//   }
+// }
+
+// const currentYear = new Date().getFullYear();
+// console.log(currentYear);
+
+// const car1 = new Car("ford", 2006);
+// car1.calculAge(currentYear);
+
+//Напиши клас Client який створює об'єкт
+//з ​​властивостями login, email
+//Оголоси приватні властивості #login #email,
+//доступ до яких зроби через геттер та сеттер login email
+
+// class Client {
+//   #login;
+//   #email;
+//   constructor(login, email) {
+//     this.#login = login;
+//     this.#email = email;
+//   }
+//   get login() {
+//     return this.#login;
+//   }
+//   get email() {
+//     return this.#email;
+//   }
+//   set login(newLogin) {
+//     return (this.#login = newLogin);
+//   }
+//   set email(newEmail) {
+//     return (this.#email = newEmail);
+//   }
+// }
+// const vasy = new Client("hero", "exampl@email.com");
+// console.log(vasy.login);
+// console.log(vasy.email);
+// vasy.login = "super";
+// console.log(vasy.login);
+
+// Створи клас Phone який створює об'єкт з ​​властивістю price.
+// Додай статичну властивість MAX_PRICE зі значенням 40000 - максимально допустима ціна телефону.
+// Оголоси приватну властивість price доступ до якої буде через геттер та сеттер.
+// Додай сетеру price перевірку значення параметра newPrice, що передається.Якщо воно більше за MAX_PRICE,
+// сеттер нічого не робить, а якщо менше або дорівнює, то перезаписує ціну телефону.
+// class Phone {
+//   static MAX_PRICE = 40000;
+//   #price;
+//   constructor(price) {
+//     this.#price = price;
+//   }
+//   get price() {
+//     return this.#price;
+//   }
+//   set price(newPrice) {
+//     if (newPrice <= Phone.MAX_PRICE) {
+//       this.#price = newPrice;
+//     }
+//   }
+// }
+// const phone1 = new Phone(20000);
+// console.log(phone1);
+// console.log(phone1.price);
+// phone1.price = 35000;
+// console.log(phone1.price);
+// phone1.price = 55000;
+// console.log(phone1.price);
+
+// Створіть клас BankAccount, який має конструктор для зберігання
+// номеру рахунку та балансу.
+// Додайте методи:
+// withdraw - метод для зняття готівки. Отримує в якості аргументу
+// суму готівки котрку потрібно зняти. Готівку можна зняти за умови,
+// що баланс більше або дорівнює сумі, котру хоче зняти користувач.
+// Виводить повідомлення: `Знято 100 грн. Залишок на рахунку: 3000 грн.`
+// або "Недостатньо коштів на рахунку."
+// deposit - метод для пововнення балансу. Отримує в якості аргументу
+// суму котру користувач хоче покласти на рахунок.
+// Виводить повідомлення: `Поповнено 100 грн. Залишок на рахунку: 3100 грн.`
+// checkBalance - метод для перевірки балансу.
+// Виводить повідомлення `Залишок на рахунку: 3100 грн.`
